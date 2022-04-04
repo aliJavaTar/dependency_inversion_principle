2cd80acf6e6a72a47d81d33cd2d757d939fdc352
alijavaboy <ali77erfagh99@gmail.com>
2022-04-04 14:47:41 +0430

    class lightBulb and ElectricPowerSwitch add:

---
+++ src/main/java/bad_example/LightBulb.java
@@ -0,0 +1,11 @@
+package bad_example;
+
+public class LightBulb {
+    public void turnOn() {
+        System.out.println("LightBulb : bulb turned on..");
+    }
+
+    public void turnOff() {
+        System.out.println("LightBulb : bulb turn Off.");
+    }
+}
